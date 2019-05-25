1d9a21129dad03de7d51047ad071dbe12e4c2e4b
Farshad <akbari.farshad@gmail.com>
2019-05-25 17:20:20 +0200

    Initial Commit

---
+++ src/main/java/com/commercetools/farshad/stockhandling/StockHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.commercetools.farshad.stockhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockHandlingApplication.class, args);
+	}
+
+}
