c87da52a5e32e19a8395bd568fa5b193f741ed27
Farshad <akbari.farshad@gmail.com>
2019-05-29 00:59:51 +0200

    - add update stock Endpoint to controller - add findAllByProductId to repository for findindg a stock of product - remove Unused method in service - add custom method to service Impl for finding or insert new Stock record

--- src/main/java/com/commercetools/farshad/stockhandling/stock/StockController.java
+++ src/main/java/com/commercetools/farshad/stockhandling/stock/StockController.java
@@ -3,8 +3,11 @@ package com.commercetools.farshad.stockhandling.stock;
 import com.commercetools.farshad.stockhandling.stock.Stock;
 import com.commercetools.farshad.stockhandling.stock.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,33 +19,16 @@ public class StockController {
     @Autowired
     private StockService stockService;
 
-    @PostMapping("/stock")
-    public Stock save(@RequestBody Stock stock){
-        return stockService.save(stock);
+    @PostMapping("/updateStock")
+    public ResponseEntity<Stock> save(@RequestBody StockModel stock) {
+        try {
+            Stock saveForUpdate = stockService.saveForUpdate(stock);
+            ResponseEntity<Stock> res = new ResponseEntity<>(saveForUpdate, HttpStatus.CREATED);
+            return res;
+        } catch (Exception ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "outdated stock, because a newer stock was processed first", ex);
+        }
     }
 
-    @GetMapping("/stock/{id}")
-    public Optional<Stock> getById(@PathVariable(value = "id") Long id){
-        return stockService.find(id);
-    }
-
-    @GetMapping("/stock")
-    public Iterable<Stock> getAll(){
-        return stockService.findAll();
-    }
-
-    @DeleteMapping("/stock/{id}")
-    public void deleteById(@PathVariable(value = "id") Long id){
-        stockService.delete(id);
-    }
-
-    @DeleteMapping("/stock")
-    public void deleteAll(){
-        stockService.deleteAll();
-    }
-
-    @GetMapping("/stock/count")
-    public long count(){
-        return stockService.count();
-    }
 }
