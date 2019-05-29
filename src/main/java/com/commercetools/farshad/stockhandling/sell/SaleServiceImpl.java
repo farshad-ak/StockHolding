30785a18a9ca4bad57d04aa7f1642782d7bce081
Farshad <akbari.farshad@gmail.com>
2019-05-29 14:15:45 +0200

    removed unused method and also add findTop3SellingProduct

--- src/main/java/com/commercetools/farshad/stockhandling/sell/SaleServiceImpl.java
+++ src/main/java/com/commercetools/farshad/stockhandling/sell/SaleServiceImpl.java
@@ -1,52 +1,20 @@
 package com.commercetools.farshad.stockhandling.sell;
 
-import com.commercetools.farshad.stockhandling.sell.Sale;
-import com.commercetools.farshad.stockhandling.sell.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class SaleServiceImpl implements SaleService {
+
 
     @Autowired
     private SaleRepository saleRepository;
 
     @Override
-    public Sale save(Sale sale) {
-        return saleRepository.save(sale);
+    public List<TopSellingProductsModel> findTop3SellingProduct() {
+        return saleRepository.findTopXSellingProduct(PageRequest.of(0, 3));
     }
-
-    @Override
-    public Optional<Sale> find(Long id) {
-        return saleRepository.findById(id);
-    }
-
-    @Override
-    public Iterable<Sale> findAll() {
-        return saleRepository.findAll();
-    }
-
-    @Override
-    public void delete(Long id) {
-    saleRepository.deleteById(id);
-    }
-
-    @Override
-    public void delete(Sale sale) {
-        saleRepository.delete(sale);
-    }
-
-    @Override
-    public void deleteAll() {
-        saleRepository.deleteAll();
-    }
-
-    @Override
-    public long count() {
-        return saleRepository.count();
-    }
-
 }
