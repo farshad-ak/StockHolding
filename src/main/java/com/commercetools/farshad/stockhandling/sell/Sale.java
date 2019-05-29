f72aa3f258127c8c440c11301a9dbcb91000181e
Farshad <akbari.farshad@gmail.com>
2019-05-29 14:12:50 +0200

    change Date datatype to LocaldateTime for better usage Java 8 DateTime Api

--- src/main/java/com/commercetools/farshad/stockhandling/sell/Sale.java
+++ src/main/java/com/commercetools/farshad/stockhandling/sell/Sale.java
@@ -1,12 +1,14 @@
 package com.commercetools.farshad.stockhandling.sell;
 
 import com.commercetools.farshad.stockhandling.product.Product;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -18,6 +20,7 @@ public class Sale {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "product_id",
             foreignKey = @ForeignKey(name = "PRODUCT_ID_FK")
@@ -25,7 +28,7 @@ public class Sale {
     private Product product;
 
     private Integer countOfItem;
-
+    private LocalDateTime saleDate;
     private Double pricePerUnit;
     private Double totalPrice;
 }
