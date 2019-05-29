b3bfefcc5ce8de3a04c9f3d4b62831aeaf5ca9db
Farshad <akbari.farshad@gmail.com>
2019-05-29 14:14:21 +0200

    removed unused method and just add findTop3SellingProduct method

--- src/main/java/com/commercetools/farshad/stockhandling/sell/SaleService.java
+++ src/main/java/com/commercetools/farshad/stockhandling/sell/SaleService.java
@@ -1,24 +1,7 @@
 package com.commercetools.farshad.stockhandling.sell;
 
-import com.commercetools.farshad.stockhandling.sell.Sale;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface SaleService {
-
-    Sale save(Sale sale);
-
-    Optional<Sale> find(Long id);
-
-    Iterable<Sale> findAll();
-
-    void delete(Long id);
-
-    void delete(Sale sale);
-
-    void deleteAll();
-
-    long count();
-
+    List<TopSellingProductsModel> findTop3SellingProduct();
 }
